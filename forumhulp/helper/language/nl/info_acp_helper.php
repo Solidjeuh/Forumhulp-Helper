c537a2871a219873be6f5570660be8e7df6b09b0
Andy Dm <demarieandy@outlook.com>
2017-04-28 00:00:10 +0200

    Add files via upload

---
+++ forumhulp/helper/language/nl/info_acp_helper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* @package Forumhulp Helper
+* @copyright (c) 2015 ForumHulp.com
+* Nederlandse vertaling @ Solidjeuh <https://www.froddelpower.be>
+* @license Proprietary
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'LOG_CORE_INSTALLED'	=> '<strong>Forumhulp Helper</strong><br />» Bestanden succesvol gewijzigd',
+	'LOG_CORE_DEINSTALLED'	=> '<strong>Forumhulp Helper</strong><br />» Bestanden succesvol hersteld',
+	'LOG_CORE_NOT_REPLACED'	=> '<strong>Forumhulp Helper</strong><br />» Kan bestand(en) niet vervangen<br />» %s',
+	'LOG_CORE_NOT_UPDATED'	=> '<strong>Forumhulp Helper</strong><br />» Kan bestand(en) niet updaten<br />» %s',
+
+	'ERROR_DISABLE'			=> 'U kan Forumhulp Helper niet uitschakelen wegens actieve Forumhulp extensies<br /><div style="margin: 0px auto; width: 50%%; text-align: left;">» %s</div>'
+));
